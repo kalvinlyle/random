1cf96c7f5f684fd36ae2e09a7baf1b934ebe0198
Kalvin <kalvinlyle@gmail.com>
2019-11-06 11:01:27 +0100

    initial commit

---
+++ random/random.lua
@@ -0,0 +1,56 @@
+-- Title: Random
+-- Author: Kalvin Lyle
+-- Version: 1.0
+-- Description: Defold module for generating random values using a variety of methods
+
+local M = {}
+
+function M.table(list) -- roll an option from a list in a weighted table formatted { Weight = num, Option = { table } }
+
+	local range = #list 				-- table range
+	local roll = math.random(1, range) 	-- random selection
+	local choice = list[roll]		-- find result in table
+
+	return choice
+end
+
+function M.table_weighted(list) -- roll an option from a list in a weighted table formatted { Weight = num, Option = { table } }
+	local range = 0
+	
+	-- build total range of options for random roll
+	for n in ipairs(list) do
+		range = range + list[n]["Weight"]
+	end 
+
+	-- roll within total range
+	local roll = math.random(1, range)
+
+	-- find result in options
+	local choice = {}
+	for n in ipairs(list) do
+		choice = list[n]["Option"]
+		if roll <= list[n]["Weight"] then break end
+		roll = roll - list[n]["Weight"]
+	end
+
+	return choice
+end
+
+function M.dice_exploding(list) -- roll an option from a list in a weighted table formatted { Die = num, Explode = num, Option = { table } }
+	local roll = 0
+	local range = 100
+	local choice = {}
+
+	-- roll in each option, if the roll in Range equals or exceeds Above escalate to the next option
+	for num in ipairs(list) do
+		roll = math.random(1, list[num].Die)
+		choice = list[num].Option
+		if roll < list[num].Explode then
+			break
+		end
+	end
+
+	return choice
+end
+
+return M
